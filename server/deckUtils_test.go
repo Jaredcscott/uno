bf616630c4a99dc43713b78b040bf45ed6b6fedd
jaredcscott <jaredcscott93@gmail.com>
2020-07-23 19:49:54 -0600

    Added tests for shuffleCards(), printCard() and printCards(). Added a function used to capture print output for these tests.

--- server/deckUtils_test.go
+++ server/deckUtils_test.go
@@ -3,6 +3,12 @@ package main
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"sync"
 )
 
 func TestGenerateShuffledDeck(t *testing.T) {
@@ -36,9 +42,48 @@ func TestShuffleCards(t *testing.T) {
 	assert.NotEqual(t, deck[:0], UnoCard{"red", "1"})
 }
 
-func TestPrintCard(t *testing.T) { 
+func TestPrintCard(t *testing.T) {
 	card := UnoCard{"red", "1"}
-	printCard(card)
-	//output //This needs to be the 'output' of the print card function
-	//assert.Equal(t,"red 1",output)
+	re := captureOutput(func() {
+		printCard(card)
+	})
+	assert.Equal(t,"red 1\n",re)
+}
+
+func TestPrintCards(t *testing.T) { 
+	deck := []UnoCard{UnoCard{"red", "1"}, UnoCard{"blue", "2"}, UnoCard{"green", "3"}}
+	re := captureOutput(func() {
+		printCards(deck)
+	})
+	assert.Equal(t,"red 1\nblue 2\ngreen 3\n",re)
+}
+
+func captureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout := os.Stdout //Storing the reference for reassinging
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
 }
