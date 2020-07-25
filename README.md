929645b925b82ab6d0adf9e0c3dc3c3e2ffd8ded
jaredcscott <jaredcscott93@gmail.com>
2020-07-24 23:12:30 -0600

    Attempting to bring master branch of my fork up to date with the most recent changes

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-Building and running Unit tests:  ![Build and Unit Testing CI](https://github.com/Jaredcscott/uno/workflows/Build%20and%20Unit%20Testing%20CI/badge.svg)
+![Go](https://github.com/jak103/uno/workflows/Go/badge.svg?branch=master)
+![Docker Image CI](https://github.com/jak103/uno/workflows/Docker%20Image%20CI/badge.svg)
+[![Coverage Status](https://coveralls.io/repos/github/jak103/uno/badge.svg?branch=dev)](https://coveralls.io/github/jak103/uno)
 
 Read me
 
-
 ## To run 
 
-`cd server/ docker-compose build`
+`cd server/ && go run .`
 
 In seperate terminal
 
-`cd server/client && npm run serve`
+`cd server/web && npm run-script build-watch`
 
 This will start the back end server, and the front end will hot-reload when editing the frontend
--- server/deckUtils_test.go
+++ server/deckUtils_test.go
@@ -3,12 +3,6 @@ package main
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-
-	"bytes"
-	"io"
-	"log"
-	"os"
-	"sync"
 )
 
 func TestGenerateShuffledDeck(t *testing.T) {
@@ -35,55 +29,4 @@ func TestGenerateShuffledDeck(t *testing.T) {
 
 	// check that the deck has the right number of total cards
 	assert.Equal(t, 108, len(deck))
-}
-
-func TestShuffleCards(t *testing.T) { 
-	deck := shuffleCards([]UnoCard{UnoCard{"red", "1"},UnoCard{"blue", "2"},UnoCard{"green", "3"}})
-	assert.NotEqual(t, deck[:0], UnoCard{"red", "1"})
-}
-
-func TestPrintCard(t *testing.T) {
-	card := UnoCard{"red", "1"}
-	re := captureOutput(func() {
-		printCard(card)
-	})
-	assert.Equal(t,"red 1\n",re)
-}
-
-func TestPrintCards(t *testing.T) { 
-	deck := []UnoCard{UnoCard{"red", "1"}, UnoCard{"blue", "2"}, UnoCard{"green", "3"}}
-	re := captureOutput(func() {
-		printCards(deck)
-	})
-	assert.Equal(t,"red 1\nblue 2\ngreen 3\n",re)
-}
-
-func captureOutput(f func()) string {
-	reader, writer, err := os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-	stdout := os.Stdout //Storing the reference for reassinging
-	stderr := os.Stderr
-	defer func() {
-		os.Stdout = stdout
-		os.Stderr = stderr
-		log.SetOutput(os.Stderr)
-	}()
-	os.Stdout = writer
-	os.Stderr = writer
-	log.SetOutput(writer)
-	out := make(chan string)
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		var buf bytes.Buffer
-		wg.Done()
-		io.Copy(&buf, reader)
-		out <- buf.String()
-	}()
-	wg.Wait()
-	f()
-	writer.Close()
-	return <-out
 }
